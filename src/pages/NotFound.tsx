2605b19bc37f523b84f80291ca86042bcd2d1f52
Gabriel Casanova <con.casanovaproductions@outlook.com>
2025-04-04 15:10:04 -0300

    Changed decoration of return button

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -11,7 +11,11 @@ const NotFound = () => {
         <h2 className="text-9xl font-walkaway">404</h2>
         <p className="text-2xl font-fredoka">{ t("pages.notfound.text") }</p>
 
-        <Link className="mt-7 font-fredoka hover:font-bold" to="/" >{ t("pages.notfound.return") }</Link>
+        <Link className="mt-7 font-fredoka text-blue-500 text-center xl:text-left 
+                         hover:font-bold hover:underline" 
+                         to="/" >
+          { t("pages.notfound.return") }
+        </Link>
       </div>
     </Page>
   )
