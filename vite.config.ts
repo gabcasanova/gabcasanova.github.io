eaf3b887bcfd7cd46dbb05266b73c2a590a41946
Gabriel Casanova <con.casanovaproductions@outlook.com>
2026-09-02 23:05:09 -0300

    Image optimization

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,26 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/',
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(), 
+    tailwindcss(),
+    ViteImageOptimizer({
+        png: { quality: 80 },
+        jpeg: { quality: 50 },
+        jpg: { quality: 50 },
+        webp: { quality: 80 },
+        avif: { quality: 70 },
+        svg: {
+            plugins: [
+                { name: 'removeViewBox' },
+                { name: 'sortAttrs' },
+            ],
+        },
+    }),
+  ],
 })
